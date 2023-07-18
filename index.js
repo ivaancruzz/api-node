4326c5f0ebd45336267080bed144c7bb424e16c0
Iván Nicolás <ivannicolascruz01@gmail.com>
2023-07-18 16:07:55 -0300

    Implement products

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
+const fetch = require("cross-fetch")
 const express = require("express")
 const app = express()
 const port = 3000
 app.use(express.json())
 
-app.post("/compuya", (req, res) => {
-  console.log(req)
-  console.log(res)
-  res.send(
-    JSON.stringify({
-      Message: "Mensaje que se enviará al contacto",
-      Type: 0,
-      TypeUrl:
-        "https://media.revistagq.com/photos/5e1c38f56ebc30000824edeb/16:9/w_1920,c_limit/baby%20yoda%20yoda%20relacion%20jon%20favreau.jpg",
-      Action: 1,
-      Tags: [],
-      Note: "",
-    })
-  )
+app.post("/compuya", async (req, res) => {
+  const body = req.body
+
+  const products = await getProducts()
+
+  res.send(sendMessageAllProducts(products))
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+async function getProducts() {
+  return new Promise((resolve, reject) => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => {
+        res.json().then((data) => {
+          resolve(data.products)
+        })
+      })
+      .catch((e) => {
+        reject(e)
+      })
+  })
+}
+
+function sendMessageAllProducts(products, res) {
+  let displayMessage = ""
+  products.forEach((product, index) => {
+    displayMessage += `${product.title}\n${product.description}\nPrecio: ${product.price}\n\nResponde: ${index} para ver más\n\n\n`
+  })
+  return JSON.stringify({
+    Message: displayMessage,
+    Type: 4,
+    TypeUrl: null,
+    Action: 1,
+    Tags: [],
+    Note: "",
+  })
+}
