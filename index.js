0207d9a11f7c25f9e2c792fa927a70ffc86cfa64
Iván Nicolás <ivannicolascruz01@gmail.com>
2023-07-18 15:32:24 -0300

    Test api 1

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express")
 const app = express()
 const port = 3000
+app.use(express.json())
 
-app.get("/compuya", (req, res) => {
-  res.send("Hello World!")
+app.post("/compuya", (req, res) => {
+  console.log(req.body)
+  res.send(
+    JSON.stringify({
+      Message: "Mensaje que se enviará al contacto",
+      Type: 0,
+      TypeUrl:
+        "https://media.revistagq.com/photos/5e1c38f56ebc30000824edeb/16:9/w_1920,c_limit/baby%20yoda%20yoda%20relacion%20jon%20favreau.jpg",
+      Action: 1,
+      Tags: [],
+      Note: "",
+    })
+  )
 })
 
 app.listen(port, () => {
