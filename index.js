31ca36977deeb362fde3f2a9b8cff76eb9184f58
Iván Nicolás <ivannicolascruz01@gmail.com>
2023-07-19 11:28:46 -0300

    test wait

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ app.post("/", async (req, res) => {
 
   // await flow.startFlow(res, body)
 
-  res.send(test.testSendImage())
+  // res.send(test.testSendImage())
   // res.send(test.testSendVideo())
   // res.send(test.testSendAudio())
   // res.send(test.testSendFile())
   // res.send(test.testSendText())
   // res.send(test.testActionContinue())
-  // res.send(test.testActionWait())
+  res.send(test.testActionWait())
   // res.send(test.testActionAssignOperator())
   // res.send(test.testActionEndConversation())
   // res.send(test.testTags())
